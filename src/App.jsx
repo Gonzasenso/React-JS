05fb8f1e354400066ddacda758716b3940d9645b
Gonzalo Asenso <Gonzacabj100@gmail.com>
2023-10-31 04:58:09 -0300

    NavBar creado

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBarComponent from "./components/NavBarComponent/NavBarComponent";
+import "./index.css";
+
+
+
+function App() {
+
+  return (
+    <div>
+      <NavBarComponent />
+    </div>
+  )
+
+
+}
+
+export default App;
